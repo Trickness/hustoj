23b014ce44c414939fb071e4716aa1468c473d8f
张峻锋 <SternWZhang@outlook.com>
2014-09-04 13:36:07 +0800

    Delete lang set

--- beta/web/template/bs/oj-footer.php
+++ beta/web/template/bs/oj-footer.php
@@ -1,13 +1,13 @@
 
 <center>
 	<p>
-	/** <?php if(file_exists("setlang.php")){?>
-		<a href=setlang.php?lang=ko>한국어</a>&nbsp;
-		<a href=setlang.php?lang=cn>中文</a>&nbsp;
-		<a href=setlang.php?lang=fa>فارسی</a>&nbsp;
-		<a href=setlang.php?lang=en>English</a>&nbsp;
-		<a href=setlang.php?lang=th>ไทย</a>	<br>
-	<?php }?>*/
+//	<?php if(file_exists("setlang.php")){?>
+//		<a href=setlang.php?lang=ko>한국어</a>&nbsp;
+//		<a href=setlang.php?lang=cn>中文</a>&nbsp;
+//		<a href=setlang.php?lang=fa>فارسی</a>&nbsp;
+//		<a href=setlang.php?lang=en>English</a>&nbsp;
+//		<a href=setlang.php?lang=th>ไทย</a>	<br>
+//	<?php }?>
 	
 	
 		Anything about the Problems, Please Contact Admin:<a href="mailto:<?php echo $OJ_ADMIN?>">admin</a>
